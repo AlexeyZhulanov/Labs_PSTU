492c1d02358b5af73790c3e1fa10386b93274270
AlexeyZhulanovRIS-21-1b <92538453+AlexeyZhulanov@users.noreply.github.com>
2021-10-29 16:41:21 +0500

    Add files via upload

---
+++ Олимпиадные задачи/programma2.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	srand(time(0));
+	int rnumb = rand() % 100;
+	int n, f = 5;
+	for (int i = 1; i <= 7; i++) {
+		if (f == 5) {
+			cout << "Введите число: ";
+			cin >> n;
+			if (rnumb > n) {
+				cout << "Загаданное число больше введенного" << endl;
+				cout << "Осталось попыток: " << 7 - i << endl;
+			}
+			if (rnumb < n) {
+				cout << "Загаданное число меньше введенного" << endl;
+				cout << "Осталось попыток: " << 7 - i << endl;
+			}
+			if (rnumb == n) {
+				f = 4;
+				cout << "Вы угадали!";
+			}
+		}
+	}
+	if (f == 5) {
+		cout << "Вы не угадали" << endl;
+		cout << "Загаданное число: " << rnumb;
+	}
+}
